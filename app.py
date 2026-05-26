6f05852af44eb8060780fc7cadbd292015c95c61
Jorne67 <schumaja@katharineum.de>
2026-05-26 15:38:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ uploaded_file = st.file_uploader("Lade ein Bild hoch", type=["jpg","jpeg","png"]
 if uploaded_file:
     st.image(uploaded_file)
 
-    # Bild zu base64 konvertieren
     img_bytes = uploaded_file.read()
     img_base64 = base64.b64encode(img_bytes).decode("utf-8")
 
-    # Roboflow API aufrufen
-api_key = st.secrets["ROBOFLOW_API_KEY"]
+    api_key = st.secrets["ROBOFLOW_API_KEY"]
     url = "https://classify.roboflow.com/banana-ripeness-classification/3"
-    
+
     response = requests.post(
         url,
         params={"api_key": api_key},
