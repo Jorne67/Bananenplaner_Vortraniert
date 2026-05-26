c9d4743a254befc56ca07872b4af2492b5d0750c
Jorne67 <schumaja@katharineum.de>
2026-05-26 15:18:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 import streamlit as st
-from inference_sdk import InferenceHTTPClient
+import requests
 
 st.title("🍌 Banana Ripeness Detection")
 
 uploaded_file = st.file_uploader(
-    "Lade ein Bananenbild hoch",
-    type=["jpg", "jpeg", "png"]
+    "Lade ein Bild hoch",
+    type=["jpg","jpeg","png"]
 )
 
 if uploaded_file:
 
-    with open("temp.jpg", "wb") as f:
-        f.write(uploaded_file.getbuffer())
-
-    client = InferenceHTTPClient(
-        api_url="https://serverless.roboflow.com",
-        api_key="OJPob1K0te2vvBOc175w"
-    )
-
-    result = client.run_workflow(
-        workspace_name="jarnes-workspace",
-        workflow_id="detect-and-classify-3",
-        images={
-            "image": "temp.jpg"
-        },
-        use_cache=True
-    )
-
     st.image(uploaded_file)
 
-    st.subheader("Ergebnis")
+    url = "https://serverless.roboflow.com/workflows/jarnes-workspace/detect-and-classify-3"
 
+    response = requests.post(
+        url,
+        params={
+            "api_key":"OJPob1K0te2vvBOc175w"
+        },
+        files={
+            "image": uploaded_file
+        }
+    )
+
+    result = response.json()
+
+    st.subheader("Ergebnis")
     st.json(result)
